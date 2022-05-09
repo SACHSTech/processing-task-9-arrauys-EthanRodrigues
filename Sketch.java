48c9baf148b09dba244a637c9aecfe6f3ded5146
EthanRodrigues <ethan.rodrigues23@ycdsbk12.ca>
2022-05-09 11:47:53 -0400

    change

--- Sketch.java
+++ Sketch.java
@@ -8,13 +8,10 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  
   public void setup() {
     background(210, 255, 173);
   }
