285b36d9ce910536589d0b9200e367f7afa4e108
EthanRodrigues <ethan.rodrigues23@ycdsbk12.ca>
2022-06-05 23:33:48 -0400

    Code for processing task 9

--- Sketch.java
+++ Sketch.java
@@ -1,33 +1,111 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  float[] circleY = new float[10];
+  float[] circleX = new float[10];
+  float circleSpeed = 1;
+  boolean[] ballHideStatus = new boolean[10];
+
+  float playerCircleX = 200;
+  float playerCircleY = 350;
+  int intLives = 3;
+
   public void settings() {
-	// put your size call here
-    size(500, 500);
+    size(400, 400);
   }
 
-  
   public void setup() {
-    background(210, 255, 173);
+    background(212, 244, 255);
+    for (int i = 0; i < circleX.length; i++){
+      circleX[i] = random(width);
+      circleY[i] = random(height);
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(212, 244, 255);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if (keyPressed){
+      if (keyCode == UP && circleSpeed > 0.1){ 
+        circleSpeed = (float)(circleSpeed - 0.1);
+      }
+      else if (keyCode == DOWN){
+        circleSpeed = (float)(circleSpeed + 0.1);
+      }
+    }
+
+    fill(36, 36, 201);
+    ellipse(playerCircleX, playerCircleY, 10, 10);
+
+    if (keyPressed) {
+      if (key == 'w') {
+        playerCircleY -= 1.5;
+      } 
+      else if (key == 'a') {
+        playerCircleX -= 1.5;
+      } 
+      else if(key == 's'){
+        playerCircleY += 1.5;
+      }
+      else if(key == 'd'){
+        playerCircleX += 1.5;
+        }
+      } 
+       
+    for (int i = 0; i < circleY.length; i++){
+      if (ballHideStatus[i] == false){
+        fill(255);
+        ellipse(circleX[i], circleY[i], 30, 30);
+        circleY[i] = circleY[i] + circleSpeed;
+      }
+      
+      if (circleY[i] >= height){
+        circleY[i] = 0;
+      }
+      
+      if ((playerCircleX <= circleX[i] + 20) && (playerCircleX >= circleX[i] - 20)){
+        if((playerCircleY <= circleY[i] + 20) && (playerCircleY >= circleY[i] - 20)){
+          intLives = intLives - 1;
+          ballHideStatus[i] = true;
+        }
+      }
+    
+      if (mousePressed){
+        if ((mouseX <= circleX[i] + 20) && (mouseX >= circleX[i] - 20)){
+          if((mouseY <= circleY[i] + 20) && (mouseY >= circleY[i] - 20)){
+            ballHideStatus[i] = true;
+          }
+        }
+      }
+    }
+    
+    if(intLives == 3){
+      fill(240, 0, 24);
+      rect(280, 10, 30, 30);
+      rect(320, 10, 30, 30);
+      rect(360, 10, 30, 30);
+    
+    }else if(intLives == 2){
+      fill(240, 100, 100);
+      rect(320, 10, 30, 30);
+      rect(360, 10, 30, 30);
+    
+    }else if(intLives == 1){
+      fill(240, 200, 200);
+      rect(360, 10, 30, 30);
+
+    }else if(intLives < 1){
+      background(255);  
+    }
   }
   
-  // define other methods down here.
+  public void mousePressed() {
+    for (int i = 0; i < circleY.length; i++){
+      if ((mouseX <= circleX[i] + 20) && (mouseX >= circleX[i] - 20)){
+        if((mouseY <= circleY[i] + 20) && (mouseY >= circleY[i] - 20)){
+          ballHideStatus[i] = true;
+        }
+      }
+    }
+  }
 }
